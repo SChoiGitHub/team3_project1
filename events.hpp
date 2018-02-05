81135ebddfd20c4c6ee1796883ee4220ce39fd4e
Brian Quiroz <r229q057@ku.edu>
2018-02-04 20:01:09 -0500

    modified createEvent function in events.hpp so that it checks for a valid date input

--- events.hpp
+++ events.hpp
@@ -37,15 +37,59 @@ void Events::createEvent()
 	
 	std::string eventName;
 	std::string date;
+	bool dateIsCorrect = true;
 	std::string timeSlot;
 	
 	std::cout << "Enter event name: ";
-	std::getline(std::cin,eventName);
-	std::cout << "Enter date: ";
-	std::getline(std::cin,date);
+	std::getline(std::cin, eventName);
+	
+	do {
+		dateIsCorrect = true;
+		std::cout << "Enter date: ";
+		std::getline(std::cin,date);
+		if (date.length() != 10) {
+			//std::cout << "Error -1\n";
+			dateIsCorrect = false;
+		} else if ((date[0] != '0') && (date[0] != '1')) {
+			//std::cout << "Error 0\n";
+			dateIsCorrect = false;
+		} else if ((date[0] == '0') && ((date[1] == '0') || (!isdigit(date[1])))) {
+			//std::cout << "Error 1\n";
+			dateIsCorrect = false;
+		} else if ((date[0] == '1') && (date[1] != '0') && (date[1] != '1') && (date[1] != '2')) {
+			//std::cout << "Error 2\n";
+			dateIsCorrect = false;
+		} else if (date[2] != '/') {
+			//std::cout << "Error 3\n";
+			dateIsCorrect = false;
+		} else if ((date[3] != '0') && (date[3] != '1') && (date[3] != '2') && (date[3] != '3')) {
+			//std::cout << "Error 4\n";
+			dateIsCorrect = false;
+		} else if (((date[3] == '0') || (date[3] == '1') || (date[3] == '2')) && (!isdigit(date[4]))) {
+			//std::cout << "Error 5\n";
+			dateIsCorrect = false;
+		} else if ((date[3] == '3') && (date[4] != '0') && (date[4] != '1')) {
+			//std::cout << "Error 6\n";
+			dateIsCorrect = false;
+		} else if (date[5] != '/') {
+			//std::cout << "Error 7\n";
+			dateIsCorrect = false;
+		} else if((!isdigit(date[6])) || (!isdigit(date[7])) || (!isdigit(date[8])) || (!isdigit(date[9]))) {
+			//std::cout << "Error 8\n";
+			dateIsCorrect = false;
+		}
+		
+		if (!dateIsCorrect) {
+			std::cout << "Wrong input! Please enter a valid date in MM/DD/YYYY format.\n";
+		}
+		
+	} while (!dateIsCorrect);
+	
+	std::cout << "Your date is: " << date << '\n';
+	
 	std::cout << "Enter time slot: ";
 	std::getline(std::cin,timeSlot);
-	std::cout << "Attendees";
+	std::cout << "Attendees: ";
 
 	//Add all string together with comma delimiter, line in event.list format, and then put it in io.new_line
 	//io.new_line = ...
