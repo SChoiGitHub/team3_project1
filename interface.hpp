a8e2743d595cfd94a59da45ee99f64765fa0ebec
SiluoFeng <s682f720@ku.edu>
2018-02-05 15:09:09 -0600

    Time format functions developed, improvements made

--- interface.hpp
+++ interface.hpp
@@ -1,35 +1,35 @@
 /**
-*	@author Team 3
-*	@date 
-*	@file interface.hpp
-*/
+ *    @author Team 3
+ *    @date
+ *    @file interface.hpp
+ */
 
 
 Interface::Interface()
 {
-
+    
 }
 
 
-Interface::~Interface() 
+Interface::~Interface()
 {
-	
+    
 }
 
 void Interface::clearScreen()
 {
-	std::cout << "\033[2J\033[1;1H";
+    std::cout << "\033[2J\033[1;1H";
 }
 
 std::string Interface::getInput(const char *message)
 {
-
-	std::string inputString;
-
-	std::cout << message;
-        getline(std::cin, inputString);
-      
-	return inputString;
+    
+    std::string inputString;
+    
+    std::cout << message;
+    getline(std::cin, inputString);
+    
+    return inputString;
 }
 
 
@@ -42,92 +42,97 @@ void Interface::Wait(const char * wait_string)
 
 Interface::Menu::Menu(const std::vector<std::pair<std::string, void (*)()>> &options)
 {
-	option_list = options;
+    option_list = options;
 }
 
 
 void Interface::Menu::Loop()
 {
-	while (true)
-	{
-		clearScreen();
-		
-		//Draw the menu
-		Draw();
-
-		//Get user selection
-		auto key = getInput("\nMenu -> ");
-
-		unsigned int pos = atoi(key.c_str());
-
-		//If input is valid
-		if( pos <= option_list.size() && pos > 0)
-		{
-			//.second is the second element of the pair of the vector element
-			auto func_ptr = (option_list[pos-1].second);
-
-			//If the func_ptr is a nulllptr then we exit the program (exit option)
-			if (func_ptr == nullptr) 
-			{
-				break; 
-			}
-			else 
-			{
-				func_ptr();
-			}
-		}
-		else
-		{
-			clearScreen();
-			Header();
-
-			std::cout << "This option is not valid." << std::endl;
-			Wait();
-		}
-	}
+    while (true)
+    {
+        clearScreen();
+        
+        //Draw the menu
+        Draw();
+        
+        //Get user selection
+        auto key = getInput("\nMenu -> ");
+        
+        unsigned int pos = atoi(key.c_str());
+        
+        //If input is valid
+        if( pos <= option_list.size() && pos > 0)
+        {
+            //.second is the second element of the pair of the vector element
+            auto func_ptr = (option_list[pos-1].second);
+            
+            //If the func_ptr is a nulllptr then we exit the program (exit option)
+            if (func_ptr == nullptr)
+            {
+                break;
+            }
+            else
+            {
+                func_ptr();
+            }
+        }
+        else
+        {
+            clearScreen();
+            Header();
+            
+            std::cout << "\nThis option is not valid." << std::endl;
+            Wait();
+        }
+    }
 }
 
 void Interface::Menu::Draw()
 {
-	std::string SOMETHING = "";
-
-	Header();
-
-	for (size_t i = 0; i < option_list.size(); ++i)
-		std::cout << "\n(Press " << i+1 << ")    " << option_list[i].first;
-
+    std::string SOMETHING = "";
+    
+    Header();
+    
+    for (size_t i = 0; i < option_list.size(); ++i)
+        std::cout << "\n(Press " << i+1 << ")    " << option_list[i].first;
+    
 }
 
 void Interface::Menu::Header()
 {
-
-	std::cout << "***************************************************************************\n";
-	std::cout << " ______               _      _____      _              _       _           \n"
-		     "|  ____|             | |    / ____|    | |            | |     | |          \n"
-		     "| |____   _____ _ __ | |_  | (___   ___| |__   ___  __| |_   _| | ___ _ __ \n"
-		     "|  __\\ \\ / / _ \\ '_ \\| __|  \\___ \\ / __| '_ \\ / _ \\/ _` | | | | |/ _ \\ '__|\n"
-		     "| |___\\ V /  __/ | | | |_   ____) | (__| | | |  __/ (_| | |_| | |  __/ |   \n"
-		     "|______\\_/ \\___|_| |_|\\__| |_____/ \\___|_| |_|\\___|\\__,_|\\__,_|_|\\___|_|" << std::endl;
-	std::cout << "\n***************************************************************************\n";
-	std::cout << "   Developed by: Team 3 \tEECS 443 \tProject 1 \tKU - 2018\n";
-	std::cout << "***************************************************************************\n\n";
-
-	/*ADD SECTION PRINTING ALL EVENT LIST FILE ENTRIES -> USE IO*/
-	IO io("event.list");
-	io.displayEntries();
-
-	std::cout << "***************************************************************************";
+    
+    std::cout << "***************************************************************************\n";
+    std::cout << " ______               _      _____      _              _       _           \n"
+    "|  ____|             | |    / ____|    | |            | |     | |          \n"
+    "| |____   _____ _ __ | |_  | (___   ___| |__   ___  __| |_   _| | ___ _ __ \n"
+    "|  __\\ \\ / / _ \\ '_ \\| __|  \\___ \\ / __| '_ \\ / _ \\/ _` | | | | |/ _ \\ '__|\n"
+    "| |___\\ V /  __/ | | | |_   ____) | (__| | | |  __/ (_| | |_| | |  __/ |   \n"
+    "|______\\_/ \\___|_| |_|\\__| |_____/ \\___|_| |_|\\___|\\__,_|\\__,_|_|\\___|_|" << std::endl;
+    std::cout << "\n***************************************************************************\n";
+    std::cout << "   Developed by: Team 3 \tEECS 443 \tProject 1 \tKU - 2018\n";
+    std::cout << "***************************************************************************\n";
+    std::cout << "        All times displayed are CDT (Central Daylight Time)\n";
+    std::cout << "***************************************************************************\n\n";
+    
+    /*ADD SECTION PRINTING ALL EVENT LIST FILE ENTRIES -> USE IO*/
+    IO io("event.list");
+    io.displayEntries();
+    
+    std::cout << "***************************************************************************";
 }
 
 void Interface::toggleTimeFormat()
 {
-	IO io("event.list");
-
-	if( io.timeFormat == false )
-		io.timeFormat = true;
-	else
-		io.timeFormat = false;
+    IO io("event.list");
+    
+    if( io.timeFormat == false )
+        io.timeFormat = true;
+    else
+        io.timeFormat = false;
 }
+
+
+
 
 
 
--- io.h
+++ io.h
@@ -1,8 +1,8 @@
 /**
-*	@author	Team 3
-*	@date
-*	@brief A header file for Input/Output (IO) class
-*/
+ *    @author    Team 3
+ *    @date
+ *    @brief A header file for Input/Output (IO) class
+ */
 
 #ifndef IO_H
 #define IO_H
@@ -15,60 +15,62 @@
 
 class IO
 {
-	public:
-
-	bool timeFormat = false;
-
-	/** @pre None
-	*   @post A file is opened with given name. It is created if it does not exist yet
-	*/
-	IO(const std::string fileName);
-
-	/** @pre None
-	*   @post A file is closed with given name
-	*/
-	~IO();
-
-	/** @pre None
-	*   @post Adds new event to file
-	*   @return None
-	*/
-	void addEntry(std::string store);
-
-	/** @pre ID is the event's unique identifier. elementName is the name of the element to retrieve
-	*   @post Retrieves some event's specific element value from the events file
-	*   @return Returns event's specific element (string or int)
-	*/
-	std::string retrieveElement(int ID, std::string elementName);
-
-	/** @pre ID is the event's unique identifier. elementName is the name of the element to update, value is
-	*	 the element's new value
-	*   @post Updates some event's specific element
-	*   @return None
-	*/
-	void updateElement(int ID, std::string elementName, void* value);
-
-	/** @pre None
-	*   @post Displays all elements in file, one at a time
-	*   @return None
-	*/
-	void displayEntries();
-
-	private:
-
-	/** @pre slot has the time value that needs to be formatted
-	*   @post Formats time between 12-hour format and 24-hour format
-	*   @return String with slot in new time format
-	*/
-	std::string timeFormatter(std::string slot);
-
-	int size;		//Number of entries in file
-	std::fstream file;		//File stream
-	std::string fileName_;		//Name of file
-	Log logFile;			//Log object
-
+public:
+    
+    static bool timeFormat;
+    
+    /** @pre None
+     *   @post A file is opened with given name. It is created if it does not exist yet
+     */
+    IO(const std::string fileName);
+    
+    /** @pre None
+     *   @post A file is closed with given name
+     */
+    ~IO();
+    
+    /** @pre None
+     *   @post Adds new event to file
+     *   @return None
+     */
+    static void addEntry();
+    
+    /** @pre ID is the event's unique identifier. elementName is the name of the element to retrieve
+     *   @post Retrieves some event's specific element value from the events file
+     *   @return Returns event's specific element (string or int)
+     */
+    std::string retrieveElement(int ID, std::string elementName);
+    
+    /** @pre ID is the event's unique identifier. elementName is the name of the element to update, value is
+     *     the element's new value
+     *   @post Updates some event's specific element
+     *   @return None
+     */
+    void updateElement(int ID, std::string elementName, void* value);
+    
+    /** @pre None
+     *   @post Displays all elements in file, one at a time
+     *   @return None
+     */
+    void displayEntries();
+    
+    
+    /** @pre slot has the time value that needs to be formatted
+     *   @post Formats time between 12-hour format and 24-hour format
+     *   @return String with slot in new time format
+     */
+    std::string timeFormatter(std::string slot);
+    
+private:
+    
+    int size;        //Number of entries in file
+    std::fstream file;        //File stream
+    std::string fileName_;        //Name of file
+    Log logFile;            //Log object
+    
 };
 
 #include "io.hpp"
 
 #endif
+
