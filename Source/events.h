b6f2f31205b8e0d1b1ba41268cf411e8940c5cff
Sherman Choi <22206740+SChoiGitHub@users.noreply.github.com>
2018-02-28 12:23:36 -0600

    Static-ified the interface variables

--- Source/events.h
+++ Source/events.h
@@ -16,52 +16,58 @@
 class Events
 {
 	public:  
-	/** @pre None
-	*   @post Start User Mode
-	*   @return None
-	*/
-	static void userMode();
+    /** @pre None
+    *   @post Start User Mode
+    *   @return None
+    */
+    static void userMode();
 
-	/** @pre None
-	*   @post Start Admin Mode
-	*   @return None
-	*/
-	static void adminMode();
-  
-  
-  static time_t t; /**Time pointer*/
-	static tm* timePtr; /**Time pointer*/
-	static int current_year; /**The current year of the execution*/
-	static int current_month; /**The current month of the execution*/
-	static int current_day; /**The current day of the execution*/
+    /** @pre None
+    *   @post Start Admin Mode
+    *   @return None
+    */
+    static void adminMode();
+    
+    
+    static time_t t; /**Time pointer*/
+    static tm* timePtr; /**Time pointer*/
+    static int current_year; /**The current year of the execution*/
+    static int current_month; /**The current month of the execution*/
+    static int current_day; /**The current day of the execution*/
+    static Interface interface; /**The interface, or UI.*/
 
 	private:
 
-	/** @pre None
-	*   @post Sets user's availability for a specific event
-	*   @return None
-	*/
-	static void setAvailability();
+    /** @pre None
+    *   @post Sets user's availability for a specific event
+    *   @return None
+    */
+    static void setAvailability();
 
-	/** @pre None
-	*   @post Creates new event specified by user
-	*   @return None
-	*/
-	static void createEvent();
-  
-  /**This method refers to the menu where someone takes a task from an event.
-	 * @pre None
-	 * @post lets an user take a task.
-	 */  
-  static void takeTask();
-  
-	/**This method deals with the UI regarding the tasks.
-	 * @pre None
-	 * @post None
-	 * @return A string that represents the tasks.
-	 */
-	static std::list<std::string> requestTasks();
-  
+    /** @pre None
+    *   @post Creates new event specified by user
+    *   @return None
+    */
+    static void createEvent();
+    
+    /**This method refers to the menu where someone takes a task from an event.
+     * @pre None
+     * @post lets an user take a task.
+     */  
+    static void takeTask();
+    
+    /**This method deals with the UI regarding the tasks.
+     * @pre None
+     * @post None
+     * @return A string that represents the tasks.
+     */
+    static std::list<std::string> requestTasks();
+    /**This method deals with the UI regarding a single date.
+     * @pre None
+     * @post None
+     * @return A string representing the date
+     */
+    static std::string requestDate();
   
   
   
