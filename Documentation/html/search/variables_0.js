5f5d29b0d13a00c313a0dcd64080d4d5a1d17f45
William Graham <wgraham@ku.edu>
2018-03-04 17:44:05 -0600

    Updated documentation

--- Documentation/html/search/variables_0.js
+++ Documentation/html/search/variables_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['clearscreen',['clearScreen',['../classInterface.html#af92bb2aeecc6a19095af23fa78b49451',1,'Interface']]],
   ['current_5fday',['current_day',['../classEvents.html#acd42b7f13cdfc41a0a6c3c6fb399da2a',1,'Events']]],
   ['current_5fmonth',['current_month',['../classEvents.html#a8a522493c1697161ca70c39232c7464b',1,'Events']]],
   ['current_5fyear',['current_year',['../classEvents.html#a772f653c845f4eedf58ba446b7fee45a',1,'Events']]]
