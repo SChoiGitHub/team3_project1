ec160d33909bb7454f05f3c3cf654f818c40a907
William Graham <wgraham@ku.edu>
2018-03-04 17:44:05 -0600

    Updated documentation

--- Documentation/html/search/functions_8.js
+++ Documentation/html/search/functions_8.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['obtainattendees',['obtainAttendees',['../classIO.html#ab9ac9f637e042cd7711cfdbc4dcbe540',1,'IO']]],
+  ['obtainattendees',['obtainAttendees',['../classIO.html#a6e49698e267ae002df56bde7875107b0',1,'IO']]],
   ['obtainevent',['obtainEvent',['../classIO.html#a1a083567d802a67de01512353a64882f',1,'IO']]],
   ['obtainschedule',['obtainSchedule',['../classIO.html#a2d8629709827d3797b4f26b9ef108f03',1,'IO']]],
-  ['obtainschedules',['obtainSchedules',['../classIO.html#ae36363fbeda79e6e3f962d9e985eb23b',1,'IO']]]
+  ['obtainschedules',['obtainSchedules',['../classIO.html#ae36363fbeda79e6e3f962d9e985eb23b',1,'IO']]],
+  ['obtaintasks',['obtainTasks',['../classIO.html#af2c5dffa84ae0741d41b0a3ccf5990ae',1,'IO']]]
 ];
